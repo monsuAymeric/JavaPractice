1593cf28521d565eed2f4d08d20eeaa1f160a405
monsuAymeric <monsu.aymeric@gmail.com>
2023-12-10 22:20:11 +0100

    feat : Add LoopOneToHundred.java (Exo 9 - TP1)

---
+++ src/tp_1/LoopOneToHundred.java
@@ -0,0 +1,13 @@
+package tp_1;
+
+public class LoopOneToHundred {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i = 1; i <= 100; i++) {
+			System.out.print(i + "\n");
+		}
+	}
+
+}
