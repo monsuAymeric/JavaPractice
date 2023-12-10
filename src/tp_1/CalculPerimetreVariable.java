f4377ea1c00c69f2693f76e3cf833840b2e70709
monsuAymeric <monsu.aymeric@gmail.com>
2023-12-10 11:57:30 +0100

    feat : Add CalculPerimetreVariable.java (Exo 3 - TP1)

---
+++ src/tp_1/CalculPerimetreVariable.java
@@ -0,0 +1,19 @@
+package tp_1;
+
+public class CalculPerimetreVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//Déclaration du rayon
+		int rayon = 10;
+		
+		// Calcul du périmètre dans une variable
+		double perimetre = 2 * Math.PI * rayon;
+		
+		//Sortie
+		System.out.println("Le périmètre est égal à " + perimetre);
+	}
+
+}
