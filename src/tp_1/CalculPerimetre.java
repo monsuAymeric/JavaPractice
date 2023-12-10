d488771ca4ab4fa50673d2d6ab71033df104a464
monsuAymeric <monsu.aymeric@gmail.com>
2023-12-10 11:40:29 +0100

    docs : Adding comments on CalculPerimetre.java (Exo 4 - TP1)

--- src/tp_1/CalculPerimetre.java
+++ src/tp_1/CalculPerimetre.java
@@ -7,13 +7,16 @@ public class CalculPerimetre {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// Récupération du rayon
 		Scanner lectureClavier = new Scanner(System.in);
 		System.out.println("Entrez un rayon : ");
 		int rayon = lectureClavier.nextInt();
 		lectureClavier.close();
 		
+		// Calcul du périmètre dans une variable
 		double perimetre = 2 * Math.PI * rayon;
 		
+		//Sortie
 		System.out.println("Le périmètre est égal à " + perimetre);	
 	}
 
