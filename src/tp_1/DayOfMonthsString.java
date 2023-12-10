783ec6e8afcec064f514f952a059a54f72eb9ea9
monsuAymeric <monsu.aymeric@gmail.com>
2023-12-10 22:04:45 +0100

    fix : function 'isBissextile' bug (Exo 8 TER - TP1)

--- src/tp_1/DayOfMonthsString.java
+++ src/tp_1/DayOfMonthsString.java
@@ -7,10 +7,10 @@ public class DayOfMonthsString {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// Récupération de l'année
-		Scanner selectMonths = new Scanner(System.in);
-		System.out.println("Sélectionner le mois : ");
-		String months = selectMonths.next();
+		String months;
+		Scanner lectureClavier = new Scanner(System.in);
+		System.out.print("Sélectionner le mois : ");
+		months = lectureClavier.nextLine();
 		
 		int dayNumber;
 		
@@ -33,27 +33,28 @@ public class DayOfMonthsString {
 				break;
 				
 			case "Février":
-				// Vérifier si l'année est bissextile
-				Scanner selectYear = new Scanner(System.in);
-				System.out.println("Sélectionner l'année : ");
-				int year = selectYear.nextInt();
-				
-				if(year % 4 == 0 && year % 400 != 0 || year % 400 == 0) {
-					dayNumber = 29;
-				}
-				else {
-					dayNumber = 28;
-				}
+				int year;
+				System.out.print("Sélectionner l'année : ");
+				year = lectureClavier.nextInt();
+				dayNumber = isBissextile(year) ? 29 : 28;
+				lectureClavier.close();
 				break;
 				
 			default:
 				System.out.println("Saisie invalide");
+				lectureClavier.close();
 				return;
 		}
 		
-		selectMonths.close();
-		
 		System.out.println("Le mois de " + months + " possède " + dayNumber + " jours");
 	}
-
+	
+	public static boolean isBissextile(int year) {
+		if(year % 4 == 0 && year % 400 != 0 || year % 400 == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
