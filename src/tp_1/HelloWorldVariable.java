049f2ef40fc17322e3c0d211189e5f086db3f3a1
monsuAymeric <monsu.aymeric@gmail.com>
2023-12-10 11:01:54 +0100

    feat add HelloWorldVariable.java (Exo 2 - TP1)

---
+++ src/tp_1/HelloWorldVariable.java
@@ -0,0 +1,12 @@
+package tp_1;
+
+public class HelloWorldVariable {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String helloWorld = "Hello World!";
+		
+		System.out.println(helloWorld);
+		}
+
+}
