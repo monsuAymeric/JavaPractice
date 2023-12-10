f55faa5aa537caea4cb464eff20a9cefa60bb1f7
monsuAymeric <monsu.aymeric@gmail.com>
2023-12-10 22:35:29 +0100

    feat : Add LoopChar.java (Exo 11 - TP1)

---
+++ src/tp_1/LoopChar.java
@@ -0,0 +1,14 @@
+package tp_1;
+
+public class LoopChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i = 1; i <= 255; i++) {
+			char caractere = (char) i;
+            System.out.println(i + " = " + caractere + "\n");
+		}
+	}
+
+}
